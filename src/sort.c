709c77378d4bdb7a9b8e2e6e71505129c4615ea2
lbiasuz <biasuz.l.leonardo@gmail.com>
2023-01-17 07:52:49 -0300

    Adicionados prints, len 4 passando numero de operações

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: lbiasuz <lbiasuz@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 06:49:11 by lbiasuz           #+#    #+#             */
-/*   Updated: 2023/01/17 07:35:20 by lbiasuz          ###   ########.fr       */
+/*   Updated: 2023/01/17 07:48:27 by lbiasuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,10 @@ void sort_size_3(t_list **a)
 void sort_size_5(t_list **a, t_list **b)
 {
 	while (ft_lstsize(*a) > 3)
+	{
 		push(a, b);
+		ft_printf("pa\n");
+	}
 	sort_size_3(a);
 	if (ft_lstsize(*b) > 1)
 		sort_size_3(b);
@@ -44,18 +47,30 @@ void sort_size_5(t_list **a, t_list **b)
 			stkp(*a)->index != stkp(*b)->index + 1 
 			&& stkp(*b)->index < ft_lstsize(*a)
 			)
+		{
 			rotate(a);
+			ft_printf("ra\n");
+		}
 		else
+		{
 			push(b, a);
+			ft_printf("pb\n");
+		}
 	}
 	if (stkp(*a)->index < ft_lstsize(*a) / 2)
 	{
 		while (stkp(*a)->index != 0)
+		{
 			reverse_rotate(a);
+			ft_printf("rra\n");
+		}
 	} 
 	else
 	{
 		while (stkp(*a)->index != 0)
+		{
 			rotate(a);
+			ft_printf("ra\n");
+		}
 	}
 }
